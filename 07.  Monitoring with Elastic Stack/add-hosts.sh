6bba6db4f43a481efeb486d291c79c3a72b6c030
x-normalize <x-normalize@protonmail.com>
2023-03-24 16:33:54 +0200

    Create add-hosts.sh

---
+++ 07.  Monitoring with Elastic Stack/add-hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "192.168.99.101 server" >> /etc/hosts
+echo "192.168.99.102 node1" >> /etc/hosts
+echo "192.168.99.103 node2" >> /etc/hosts
+echo "192.168.99.104 docker" >> /etc/hosts
