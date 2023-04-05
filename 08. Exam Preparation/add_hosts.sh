3c4f0e841362976cd634ca0e4ca68031aefcffa0
x-normalize <x-normalize@protonmail.com>
2023-04-05 09:39:18 +0300

    Create add_hosts.sh

---
+++ 08. Exam Preparation/add_hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "### Adding hosts..."
+echo "192.168.56.11 vm11.do1.exam jenkins" >> /etc/hosts
+echo "192.168.56.12 vm12.do1.exam docker" >> /etc/hosts
+echo "192.168.56.13 vm13.do1.exam promgraf" >> /etc/hosts
