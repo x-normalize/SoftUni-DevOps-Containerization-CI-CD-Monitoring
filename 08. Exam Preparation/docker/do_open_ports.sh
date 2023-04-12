3a36c7ca7ccaa48cae21e8ad071bea2233fa836b
x-normalize <x-normalize@protonmail.com>
2023-04-12 09:18:45 +0300

    Create do_open_ports.sh

---
+++ 08. Exam Preparation/docker/do_open_ports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "### Firewall - open port 80/tcp, 8080/tcp and adding http and https services ..."
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --permanent --add-port=8081/tcp
+sudo firewall-cmd --permanent --add-port=3000/tcp
+sudo firewall-cmd --permanent --add-port 9100/tcp
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --reload
+
+echo "### Check firewall and accessible ports:::"
+sudo firewall-cmd --list-all
