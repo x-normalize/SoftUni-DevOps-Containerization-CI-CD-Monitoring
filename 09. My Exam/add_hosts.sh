43693fabae93163b8978f93c8d7558c7c50c1803
Yordan Nikolov <x-normalize@protonmail.com>
2023-06-02 10:10:47 +0300

    Create add_hosts.sh

---
+++ 09. My Exam/add_hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "### Adding hosts..."
+echo "192.168.56.201 pipelines.do1.exam pipelines" >> /etc/hosts
+echo "192.168.56.202 containers.do1.exam containers" >> /etc/hosts
+echo "192.168.56.203 monitoring.do1.exam monitoring" >> /etc/hosts
