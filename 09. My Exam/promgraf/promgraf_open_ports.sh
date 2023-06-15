bba1b5ee5662cb1e41b73781d4d3c98181bf14c7
Yordan Nikolov <x-normalize@protonmail.com>
2023-06-15 10:01:15 +0300

    Create promgraf_open_ports.sh

---
+++ 09. My Exam/promgraf/promgraf_open_ports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "### Firewall - open port 80/tcp, 8080/tcp and adding http and https services ..."
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --permanent --add-port 9090/tcp
+sudo firewall-cmd --permanent --add-port 9100/tcp
+sudo firewall-cmd --permanent --add-port 9093/tcp
+sudo firewall-cmd --permanent --add-port 9323/tcp
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --reload
+
+echo "### Check firewall and accessible ports:::"
+sudo firewall-cmd --list-all
