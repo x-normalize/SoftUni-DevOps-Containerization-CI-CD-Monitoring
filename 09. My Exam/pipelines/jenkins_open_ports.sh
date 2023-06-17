48cf547efb9c7075574a93dba5b4f9d8608a2a64
Yordan Nikolov <x-normalize@protonmail.com>
2023-06-17 10:25:09 +0300

    Create jenkins_open_ports.sh

---
+++ 09. My Exam/pipelines/jenkins_open_ports.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "### Firewall - open port 80/tcp, 8080/tcp and adding http and https services ..."
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --permanent --add-port=9100/tcp
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --reload
+
+echo "### Check firewall and accessible ports:::"
+sudo firewall-cmd --list-all
